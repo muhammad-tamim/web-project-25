3e9521768938cf20a1c1ab9a81d348228b050f7e
muhammad-tamim <contact2tamim@gmail.com>
2025-10-20 15:07:26 +0600

    feat(structure): add hero

--- src/shared/components/structure/Container.jsx
+++ src/shared/components/structure/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({ children }) => {
     return (
-        <div className='max-w-[1600px] mx-auto px-4 md:px-8 lg:px-12 xl:px-[160px'>
+        <div className='max-w-[1600px] mx-auto px-4 md:px-8 lg:px-12 xl:px-[160px]'>
             {children}
         </div>
     );
--- src/shared/components/structure/Hero.jsx
+++ src/shared/components/structure/Hero.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import heroImage from '../../../assets/images/banner-img-1.png'
 
 const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
+        <div
+            className="hero min-h-[calc(100vh-237px)] mt-8 mb-[100px]"
+            style={{
+                backgroundImage:
+                    `url(${heroImage})`,
+                borderRadius: "24px",
+            }}
+        >
+            <div className="w-full h-full bg-linear-to-b from-[#0f0f0f00] to-[#0f0f0f] opacity-80 rounded-[24px]"></div>
+            <div className="hero-content flex-col text-neutral-content text-center">
+                <h2 className="xl:max-w-4xl font-extrabold text-lg md:text-3xl lg:text-4xl xl:text-5xl xl:leading-14">
+                    It avoids subjective claims or exaggeration that might raise red flags legally
+                </h2>
+                <p className='xl:max-w-[959px] text-xs md:text-sm xl:text-base font-medium'>Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.</p>
+            </div>
+        </div >
     );
 };
 
