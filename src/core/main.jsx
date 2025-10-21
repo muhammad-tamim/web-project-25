8986dc873eb2c2a5d4c263dc698f86d85db4618f
muhammad-tamim <contact2tamim@gmail.com>
2025-10-21 23:31:00 +0600

    feat: add react-hot-toast

--- src/core/main.jsx
+++ src/core/main.jsx
@@ -4,10 +4,12 @@ import { createRoot } from 'react-dom/client';
 import '../styles/index.css'
 import { RouterProvider } from 'react-router';
 import { AppRoutes } from './routes/AppRoutes';
+import { Toaster } from 'react-hot-toast';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={AppRoutes}></RouterProvider>
+    <Toaster position='top-right'></Toaster>
   </StrictMode>,
 )
