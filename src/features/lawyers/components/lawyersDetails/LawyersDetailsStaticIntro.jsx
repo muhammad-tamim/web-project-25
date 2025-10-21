1b343838cabdd9b248676d34863c132c3c3b3d32
muhammad-tamim <contact2tamim@gmail.com>
2025-10-21 22:09:07 +0600

    feat: add lawyers details static intro

---
+++ src/features/lawyers/components/lawyersDetails/LawyersDetailsStaticIntro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LawyersDetailsStaticIntro = () => {
+    return (
+        <div className='my-8 bg-primary-content/5 rounded-2xl border border-primary-content/15 text-center py-[72px]'>
+            <h1 className='font-extrabold text-[32px] pb-4'>Lawyer's Profile Details</h1>
+            <p className='font-medium text-[#14141470] max-w-[1024px] mx-auto text-sm sm:text-base px-3 md:px-6 lg:px-10 xl:px-0'>Explore a comprehensive overview of the lawyer’s expertise, qualifications, and success stories. Learn how their professional journey and commitment to excellence can help you achieve the best possible outcome in your legal matters.</p>
+        </div>
+    );
+};
+
+export default LawyersDetailsStaticIntro;
