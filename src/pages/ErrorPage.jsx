0c3253d363313f58357c540c2f50b1b3d2bba2d2
muhammad-tamim <contact2tamim@gmail.com>
2025-10-22 16:00:49 +0600

    feat: add error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from '../shared/components/structure/Navbar';
+import { Link, useLocation } from 'react-router';
 
 const ErrorPage = () => {
+    const location = useLocation();
     return (
         <div>
             <Navbar></Navbar>
-            <p>Error</p>
+            <div className='min-h-[calc(100vh-105px)] flex flex-col items-center justify-center space-y-4'>
+                <h1 className='font-bold text-5xl'>404</h1>
+                <p className='text-primary-content/70 text-2xl'>No route matches URL "{location.pathname}"</p>
+                <Link to={'/'}><button className='btn btn-primary rounded-full text-white btn-xl'>Home Page</button></Link>
+            </div>
         </div>
     );
 };
